a3129f9f91fef02be315b30e1a4a07669f3f094a
JoshuaRayTanzer <73126523+JoshuaRayTanzer@users.noreply.github.com>
2024-12-03 07:47:05 -0500

    Update Functions.R

--- Functions.R
+++ Functions.R
@@ -1826,19 +1826,11 @@ inspect_mods=function(mod=NA,
     
     if(glmmTMB==1){
       res=resid(mod)
-      dpred=mod$frame
-      if(is.null(names(ranef(mod)$cond))){
-        dpred=dpred
-      }else{
-        dpred=dpred[,-c(which(names(dpred)%in%names(ranef(mod)$cond)))]
-      }
-      hat=as.matrix(dpred)%*%as.matrix(coefficients(summary(mod))$cond[,1])
-      
       fit=c(nrow(coefficients(summary(mod))$cond),
             AIC(mod),
             BIC(mod),
             -2*logLik(mod),
-            cor(hat,hat+res)^2)
+            cor(predict(mod,re.form=NA),predict(mod,re.form=NA)+res)^2)
 
       if(is.null(X)){
         X=mod$frame
@@ -2047,19 +2039,11 @@ inspect_mods=function(mod=NA,
       
       if(glmmTMB==1){
         res=resid(mod[[m]])
-        dpred=mod[[m]]$frame
-        if(is.null(names(ranef(mod[[m]])$cond))){
-          dpred=dpred
-        }else{
-          dpred=dpred[,-c(which(names(dpred)%in%names(ranef(mod[[m]])$cond)))]
-        }
-        hat=as.matrix(dpred)%*%as.matrix(coefficients(summary(mod[[m]]))$cond[,1])
-        
         fit[,m]=c(nrow(summary(mod[[m]])$coefficients$cond),
                   AIC(mod[[m]]),
                   BIC(mod[[m]]),
                   -2*logLik(mod[[m]]),
-                  cor(hat,hat+res)^2)
+                  cor(predict(mod[[m]],re.form=NA),predict(mod[[m]],re.form=NA)+res)^2)
         if(X_flag==1){
           X[[m]]=mod[[m]]$frame
           X[[m]]=X[[m]][,2:ncol(X[[m]])]
